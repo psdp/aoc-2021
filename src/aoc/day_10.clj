1a8e5b78ba745c449b9cd7760f740e8be66bf97d
psdp <psdp.dev@gmail.com>
2021-12-12 21:12:53 +0800

    day 10 part 2

--- src/aoc/day_10.clj
+++ src/aoc/day_10.clj
@@ -27,12 +27,14 @@
 (defn match-paren? [opening closing]
   (= (get paren-pairs opening) closing))
 
-(defn find-illegal-char [input]
+(defn check-syntax [input & [autocomplete?]]
   (loop [[fst & rst] input
          openings '()]
     (cond
       (nil? fst)
-      nil
+      (if autocomplete?
+        (map paren-pairs openings)
+        nil)
 
       (contains? paren-pairs fst)
       (recur rst (conj openings fst))
@@ -41,15 +43,42 @@
       (let [[opening openings'] ((juxt peek pop) openings)]
         (if (match-paren? opening fst)
           (recur rst openings')
-          fst)))))
+          (if autocomplete?
+            nil
+            fst))))))
 
-(defn calc-points [input]
+(def find-illegal-char #(check-syntax %))
+
+(defn score-1 [input]
   (->> input
        (keep find-illegal-char)
        (map illegal-char-points)
        (reduce +)))
 
 (comment
-  (calc-points parsed-sample-input) ;; => 26397
-  (calc-points parsed-real-input) ;; => 442131
+  (score-1 parsed-sample-input) ;; => 26397
+  (score-1 parsed-real-input) ;; => 442131
+  )
+
+;;; part 2
+
+(def autocomplete #(check-syntax % true))
+
+(def char-points
+  {\) 1, \] 2, \} 3, \> 4})
+
+(defn median [coll] (nth (sort coll) (/ (count coll) 2)))
+
+(defn score-2 [input]
+  (->> input
+       (keep autocomplete)
+       (map (fn [chars]
+              (->> chars
+                   (map char-points)
+                   (reduce #(+ (* %1 5) %2)))))
+       median))
+
+(comment
+  (score-2 parsed-sample-input) ;; => 288957
+  (score-2 parsed-real-input) ;; => 3646451424
   )
